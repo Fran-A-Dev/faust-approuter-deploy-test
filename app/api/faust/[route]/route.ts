a6518c93e33907ea0e190bf1dd16fbf70901b95a
Fran Agulto <francis.agulto@wpengine.com>
2024-04-30 08:03:04 -0500

    added variable

--- app/api/faust/[route]/route.ts
+++ app/api/faust/[route]/route.ts
@@ -1,6 +1,7 @@
-import { faustRouteHandler } from '@faustwp/experimental-app-router';
-import '@/faust.config.js';
+import { faustRouteHandler } from "@faustwp/experimental-app-router";
+import "@/faust.config.js";
 
+const apiUrl = `${process.env.NEXT_PUBLIC_WORDPRESS_URL}/api/faust/token`;
 const { GET, POST } = faustRouteHandler;
 
 export { GET, POST };
